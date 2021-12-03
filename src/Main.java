3d6689026db0516b9b57abbbd22919ffa0ec1fbc
Mathis <mathis.duban@etu.umontpellier.fr>
2021-12-03 09:25:28 +0100

    add function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        Voiture d1 = new Voiture("cars",300);
+        System.out.println(d1.toString());
+d1.toString2();
     }
 }
--- src/Voiture.java
+++ src/Voiture.java
@@ -4,8 +4,41 @@ public class Voiture {
     private int position;
     private int vitesse;
 
-    public Voiture(String nom,int vitesse){
-        this.nom=nom;
-        this.vitesse=vitesse;
+    public Voiture(String nom, int vitesse) {
+        this.nom = nom;
+        this.vitesse = vitesse;
+
     }
+
+    public String toString() {
+        String texte = "nom: " + nom + "  " + "vitesse : " + String.valueOf(vitesse);
+        return texte;
+    }
+
+    public String toString2() {
+        for (int i = 0; i < position; i++) {
+            System.out.println(" ");
+        }
+        String lettre = nom;
+        char resuslt = lettre.charAt(0);
+        System.out.println(resuslt);
+        System.out.println("\n");
+        return lettre;
+    }
+
+    public String getNom() {
+        return nom;
+
+    }
+    public boolean depasse(int limite){
+        return position>=limite;
+    }
+    public void avance(){
+        for (int i=0;i<vitesse;i++)
+            System.out.println(" ");
+    }
+    public void depart(){
+position=0;
+    }
+
 }
