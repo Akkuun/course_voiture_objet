ac752552cd77f4f54632a7a58a0c7f92c9d578e0
Mathis <mathis.duban@etu.umontpellier.fr>
2021-12-03 08:31:29 +0100

    Initial commit

---
+++ src/Voiture.java
@@ -0,0 +1,11 @@
+public class Voiture {
+
+    private String nom;
+    private int position;
+    private int vitesse;
+
+    public Voiture(String nom,int vitesse){
+        this.nom=nom;
+        this.vitesse=vitesse;
+    }
+}
